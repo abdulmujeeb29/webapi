e5c0305432d653014b67b1cb10578b219d4eea71
abdulmujeeb29 <abdulmujeebtiamiyu@gmail.com>
2023-02-21 22:04:35 +0100

    commit

--- webapiapp/models.py
+++ webapiapp/models.py
@@ -42,14 +42,16 @@ def phone_number (value):
     if len(value)> 14:
         raise ValidationError('enter appropriate length of phone number ')
     
-
+states_choices =(
+    ('1','abia') , ('2','adamawa') ,('3','akwa ibom') , ('4','anambra') ,('5','bauchi') ,('6','bayelsa') ,('7','benue') ,('8','borno ') ,('9','cross river') ,('10','delta') ,('11','ebonyi') ,('12','edo' ) ,('13','ekiti') ,('14','enugu') ,('15','gombe') ,('16', 'imo') ,('17','jigawa') ,('18','kaduna') ,('19','kano') ,('20','katsina') ,('21','kebbi') ,('22', 'kogi') ,('23', 'kwara') ,('24','lagos') ,('25','nasarawa') ,('26', 'niger') ,('27','ogun') ,('28','ondo') ,('29','osun') ,('30', 'oyo') ,('31', 'plateau') ,('32', 'rivers') ,('33', 'sokoto') ,('34', 'taraba') ,('35', 'yobe ') ,('36','zamfara') 
+)
 
 
 class buisnessProfile(models.Model):
     business_name = models.CharField(max_length=50,blank=True,null=True, validators=[alphanumeric, length_name])
     company_website=models.CharField(max_length=50,blank=True,null=True ,validators=[valid_url])
     address= models.CharField(max_length=100, validators=[address_length])
-    state = models.CharField(max_length=50,blank=True)
+    state = models.CharField(max_length=50,blank=True,choices=states_choices)
     buisness_phone= PhoneNumberField(validators=[phone_number])
     email =models.EmailField(null=True)
     #buisness_phone= models.IntegerField(validators=[valid_phonenumber_format],blank=True,null=True)
