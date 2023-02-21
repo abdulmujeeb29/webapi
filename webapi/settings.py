7e3a448e540e6925ac33a178c33cf2f8753666a2
abdulmujeeb29 <abdulmujeebtiamiyu@gmail.com>
2023-02-21 20:43:55 +0100

    sql branch

--- webapi/settings.py
+++ webapi/settings.py
@@ -78,12 +78,21 @@ WSGI_APPLICATION = 'webapi.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'api_db',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'abdulmujeeb',
     }
 }
+
+
+
 
 
 # Password validation
